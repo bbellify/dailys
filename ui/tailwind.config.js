3518079265edaea06b20c0c14958cf00e5a09c41
bbellify <brianpatrickbellinger@gmail.com>
2023-06-14 22:52:34 -0700

    update kelvin

--- ui/tailwind.config.js
+++ ui/tailwind.config.js
@@ -12,12 +12,7 @@ module.exports = {
         shake: {
           "0%": { transform: "translateX(5px)" },
           "50%": { transform: "translateX(-5px)" },
-          "100%": { transform: "translateX(5pxg)" },
-          // 0: { transform: translateX(0) },
-          // 25: { transform: translateX('5px') },
-          // 50% { transform: translateX(-5px) }
-          // 75% { transform: translateX(5px) }
-          // 100% { transform: translateX(0) }
+          "100%": { transform: "translateX(5px)" },
         },
       },
       animation: {
